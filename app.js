177d33ee509b39d2fcd86ce82a08a4ebab3fe4ec
Harsha-Vardhan-24 <87610312+Harsha-Vardhan-24@users.noreply.github.com>
2022-01-09 15:41:47 +0530

    6

--- app.js
+++ app.js
@@ -63,7 +63,7 @@ passport.serializeUser(function(user, done) {
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/secrets"
+    callbackURL: "https://depression-hv.herokuapp.com/auth/google/secrets"
   },
   function(accessToken, refreshToken, profile, cb) {
       console.log(profile);
